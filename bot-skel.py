f93f1e703cb554f241ea2f7c2fe86b978d00f560
Gheorghiu Doru-Vlad <21gheorghi@gmail.com>
2021-12-09 22:47:42 +0200

    Added updated Discord bot script
    Signed-off-by: Gheorghiu Doru-Vlad <21gheorghi@gmail.com>

--- bot-skel.py
+++ bot-skel.py
@@ -1,5 +1,5 @@
 
-    
+#copyright Gheorghiu Doru-Vlad 312CA    
 
     #!./.venv/bin/python
 
@@ -9,6 +9,10 @@ import os           # environment variables
 import inspect      # call stack inspection
 import random       # dumb random number generator
 import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
 from discord import FFmpegPCMAudio
 
 from discord.ext import commands    # Bot class and utils
@@ -161,3 +165,18 @@ if __name__ == '__main__':
 
     # launch bot (blocking operation)
     bot.run(os.environ['BOT_TOKEN'])
+
+"""
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
